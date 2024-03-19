d48a966265d32c120886504ee8810d1ba87282b4
Anna Borodaienko <anna.borodaienko@gmail.com>
2024-03-19 17:03:59 +0200

    add weather API

---
+++ app/api/weather/route.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+import { DEFAULT_LOCATION } from '../../constants/defaultLocations';
+
+export async function GET() {
+  try {
+    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+    const lat = DEFAULT_LOCATION.coord.lat;
+    const lon = DEFAULT_LOCATION.coord.lon;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const res = await axios.get(url);
+    return NextResponse.json(res.data);
+  } catch (error) {
+    console.log(error);
+    return new Response('Error fetching forecast data', { status: 500 });
+  }
+}
--- app/providers/GlobalContextProvider.tsx
+++ app/providers/GlobalContextProvider.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import { GlobalContext, GlobalContextUpdate } from '../context/GlobalContext';
 
 export const GlobalContextProvider = ({
@@ -7,9 +10,30 @@ export const GlobalContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  const [forecast, setForecast] = useState({});
+  const fetchForecast = async () => {
+    try {
+      const res = await axios.get('api/weather');
+      setForecast(res.data);
+      console.log('res', res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchForecast();
+  }, []);
+
   return (
-    <GlobalContext.Provider value='hello'>
-      <GlobalContextUpdate.Provider value='hello'>
+    <GlobalContext.Provider
+      value={{
+        forecast,
+      }}>
+      <GlobalContextUpdate.Provider
+        value={{
+          forecast,
+        }}>
         {children}
       </GlobalContextUpdate.Provider>
     </GlobalContext.Provider>
